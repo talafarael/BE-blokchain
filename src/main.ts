acbf2d2dd355793594344dd5839a04c6567d51e9
artem <artemk2504@gmail.com>
2025-01-25 17:50:09 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // Replace '*' with specific domains for better security
+    methods: 'GET,POST,PUT,DELETE,PATCH',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
