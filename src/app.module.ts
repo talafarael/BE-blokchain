7999d74187848b6ec86f0a2e4af6bb0af9c698b0
artem <artemk2504@gmail.com>
2025-01-26 11:35:26 +0200

    chagne api valute

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CryptoApiModule } from './crypto-api/crypto-api.module';
 import { DbFillCryptoModule } from './db-fill-crypto/db-fill-crypto.module';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [CryptoApiModule, DbFillCryptoModule, AuthModule],
+  imports: [CryptoApiModule, DbFillCryptoModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/db-fill-crypto/db-fill-crypto.service.ts
+++ src/db-fill-crypto/db-fill-crypto.service.ts
@@ -6,15 +6,11 @@ export class DbFillCryptoService {
   constructor(private prisma: PrismaService) {}
   async fill(data: CryptoQueryPageDto) {
     try {
-      const oldPrice = data.price[0][1];
-      const newPrice = data.price[data.price.length - 1][1];
-      const percentageChange = ((newPrice - oldPrice) / oldPrice) * 100;
       const newCryptoPrice = await this.prisma.cryptoPrice.create({
         data: {
           name: data.name,
           image: data.image,
           usd: data.usd,
-          price_change_percentage_week: percentageChange,
           price: {
             create: data.price.map(([timestamp, value]) => ({
               timestamp: timestamp,
